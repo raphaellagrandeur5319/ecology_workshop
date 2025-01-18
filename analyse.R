9b152cc1c0b25d8ada0b7a468ea24f9ff6d634aa
Raphaël Lagrandeur <raphael.lagrandeur@umontreal.ca>
2025-01-18 15:27:12 -0500

    alpha.02

--- analyse.R
+++ analyse.R
@@ -1,4 +1,5 @@
 library(tidyverse)
 library(ratdat)
-ggplot(data=complete_old, aes(x=weight, y = hindfoot_length, color=sex))+geom_point()
-owkdoejdocd
+ggplot(data=complete_old, aes(x=weight, y = hindfoot_length, color=sex))+geom_point(alpha=0.2)
+
+T
